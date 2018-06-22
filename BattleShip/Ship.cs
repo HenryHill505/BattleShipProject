37167129958cd8882133367a6756f757a409ee62
HenryHill505 <hhill505@gmail.com>
2018-06-22 08:45:18 -0500

    Added method PlaceShip() to Ship, along with array variables verticalCoordinates and horizontalCoordinates

--- BattleShip/Ship.cs
+++ BattleShip/Ship.cs
@@ -8,11 +8,63 @@ namespace BattleShip
 {
     public abstract class Ship
     {
-       public string type;
-       public int hitSpaces;
+        public string type;
+        public int hitSpaces;
+        public int[] verticalCoordinates;
+        public int[] horizontalCoordinates;
 
         public Ship()
         {
+        }
+
+        public virtual void PlaceShip()
+        {
+            Console.WriteLine("Pick the x coordinate for the Ship's stern");
+            int sternVerticalCoordinate = int.Parse(Console.ReadLine());
+            Console.WriteLine("Pick the x coordinate for the ship's stern");
+            int sternHorizontalCoordinate = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Is the ship pointed up, down, left, or right");
+            string shipDirection = Console.ReadLine();
+            switch (shipDirection)
+            {
+                case "up":
+                    verticalCoordinates = new int[hitSpaces];
+                    horizontalCoordinates = new int[1];
+                    for (int i = 0; i < hitSpaces; i++)
+                    {
+                        verticalCoordinates[i] = sternVerticalCoordinate - i;
+                    }
+                    horizontalCoordinates[0] = sternHorizontalCoordinate;
+                    break;
+                case "down":
+                    verticalCoordinates = new int[hitSpaces];
+                    horizontalCoordinates = new int[1];
+                    for (int i = 0; i < hitSpaces; i++)
+                    {
+                        verticalCoordinates[i] = sternVerticalCoordinate + i;
+                    }
+                    horizontalCoordinates[0] = sternHorizontalCoordinate;
+                    break;
+                case "left":
+                    verticalCoordinates = new int[1];
+                    horizontalCoordinates = new int[hitSpaces];
+                    verticalCoordinates[0] = sternVerticalCoordinate;
+                    for (int i = 0; i < hitSpaces; i++)
+                    {
+                        horizontalCoordinates[i] = sternHorizontalCoordinate - i;
+                    }
+                    break;
+                case "right":
+                    verticalCoordinates = new int[1];
+                    horizontalCoordinates = new int[hitSpaces];
+                    verticalCoordinates[0] = sternVerticalCoordinate;
+                    for (int i = 0; i < hitSpaces; i++)
+                    {
+                        horizontalCoordinates[i] = sternHorizontalCoordinate + i;
+                    }
+                    break;
+            }
         }
     }
 }
