f802d4e989ec38a1f23e27fb5cbedf9d09cef973
HenryHill505 <hhill505@gmail.com>
2018-06-25 08:19:29 -0500

    Added Try Catch to Player.Shoot()

--- BattleShip/Player.cs
+++ BattleShip/Player.cs
@@ -18,18 +18,25 @@ namespace BattleShip
 
         public void Shoot(Player targetPlayer)
         {
-            
-            Console.WriteLine("Enter the x coordinate for your shot");
-            int horizontalCoordinate = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the y coordinate for your shot");
-            int verticalCoordinate = int.Parse(Console.ReadLine());
-            if (!targetPlayer.board.shotsTaken[verticalCoordinate, horizontalCoordinate]){
-                targetPlayer.board.ReceiveShot(verticalCoordinate, horizontalCoordinate);
-                targetPlayer.board.shotsTaken[verticalCoordinate, horizontalCoordinate] = true;
-            }
-            else
+            try
             {
-                Console.WriteLine("You have already fired at this location. Pick another set of coordinates");
+                Console.WriteLine("Enter the x coordinate for your shot");
+                int horizontalCoordinate = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter the y coordinate for your shot");
+                int verticalCoordinate = int.Parse(Console.ReadLine());
+                if (!targetPlayer.board.shotsTaken[verticalCoordinate, horizontalCoordinate])
+                {
+                    targetPlayer.board.ReceiveShot(verticalCoordinate, horizontalCoordinate);
+                    targetPlayer.board.shotsTaken[verticalCoordinate, horizontalCoordinate] = true;
+                }
+                else
+                {
+                    Console.WriteLine("You have already fired at this location. Pick another set of coordinates");
+                    Shoot(targetPlayer);
+                }
+            }
+            catch
+            {
                 Shoot(targetPlayer);
             }
         }
