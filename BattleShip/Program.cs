b163661b2c07bf1a654ce4375fd6cb679d002a33
HenryHill505 <hhill505@gmail.com>
2018-07-04 13:07:17 -0500

    Removed extraneous comments

--- BattleShip/Program.cs
+++ BattleShip/Program.cs
@@ -10,10 +10,6 @@ namespace BattleShip
     {
         static void Main(string[] args)
         {
-            //Board board = new Board(20, 20);
-            //board.PlaceShips();
-            //board.DisplayToOwner();
-
             GameMaster game = new GameMaster();
             game.RunGame();
         }
